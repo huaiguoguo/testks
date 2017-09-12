b10de618933c5fce723d77018249367eb84fa889
mason <29055903@qq.com>
2017-09-12 14:45:37 +0800

    init project

---
+++ frontend/themes/basic/site/gitbook.php
@@ -0,0 +1,183 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/9/12
+ * Time: 12:21
+ */
+?>
+
+
+
+<!DOCTYPE html>
+<!-- saved from url=(0084)https://www.gitbook.com/book/huaiguoguo/test/edit#/edit/master/chapter1.md?_k=ciffbi -->
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+    <title>第一章 — GitBook Editor</title>
+    <link rel="stylesheet" href="/gitbook/assets/application.css">
+    <link rel="icon" href="https://www.gitbook.com/editor/assets/images/logo/favicon.ico">
+    <meta name="robots" content="noindex, nofollow">
+</head>
+<body>
+<div id="MathJax_Message" style="display: none;"></div>
+<div id="application">
+    <div data-reactroot="">
+        <div class="desktop-window platform-windows RouteEdit">
+            <div class="window-container"><!-- react-empty: 7 --><!-- react-empty: 8 -->
+                <div class="window-body">
+                    <div class="EditorHeader">
+                        <div class="gb-page-header">
+                            <div class="Header-BackButton style-primary">
+                                <div class="step step-default"><i class="Icon octicon octicon-pencil"></i></div>
+                                <div class="step step-hover"><i class="Icon octicon octicon-chevron-left"></i></div>
+                            </div>
+                            <div class="Header-Context"><p class="context-title">
+                                    <a class="" href="https://www.gitbook.com/book/huaiguoguo/test/edit#">木子美日记</a>
+                                </p>
+                                <div class="VersionsControls">
+                                    <a href="https://www.gitbook.com/book/huaiguoguo/test/edit#" class="tooltipped" aria-label="Select a version to edit">Primary version</a>
+                                    <a href="https://www.gitbook.com/book/huaiguoguo/test/edit#">Create a change request</a></div>
+                            </div>
+
+                            <div class="AlertStatus"></div>
+                        </div>
+                    </div>
+                    <div class="Grid">
+                        <div class="GridEntry" style="top: 0%; left: 0%; height: 100%; width: 15%;">
+                            <div class="entry-inner with-bar-h">
+                                <div class="Sidebar">
+                                    <div class="pagehead">
+                                        <ul class="menu">
+                                            <li class="active">
+                                                <a href="https://www.gitbook.com/book/huaiguoguo/test/edit#"><span>目录</span></a>
+                                            </li>
+<!--                                            <li class=""><a href="https://www.gitbook.com/book/huaiguoguo/test/edit#">Files</a></li>-->
+                                        </ul>
+                                    </div>
+                                    <div class="Sidebar-Body">
+                                        <div class="Summary">
+                                            <div class="SummaryContent">
+                                                <div class="Tree">
+                                                    <div class="TreeEntry">
+                                                        <div class="inner" draggable="true" style="padding-left: 10px;">
+                                                            <!-- react-text: 162 --><!-- /react-text --><span
+                                                                class="name"><span class="SpanInput">介绍</span><span
+                                                                    class="SummaryEditButton"><i
+                                                                        class="Icon octicon octicon-pencil"></i></span></span>
+                                                        </div>
+                                                    </div>
+                                                    <div class="TreeEntry is-active">
+                                                        <div class="inner" draggable="true" style="padding-left: 10px;">
+                                                            <div class="icon-triangle"><i class="octicon octicon-triangle-down"></i></div>
+                                                            <!-- react-text: 169 --><!-- /react-text -->
+                                                            <span class="name">
+                                                                <span class="SpanInput">第一章</span>
+                                                                <span class="SummaryEditButton"><i class="Icon octicon octicon-pencil"></i></span>
+                                                            </span>
+                                                        </div>
+                                                        <div class="tree">
+                                                            <div class="TreeEntry">
+                                                                <div class="inner" draggable="true" style="padding-left: 28px;"><!-- react-text: 346 --><!-- /react-text -->
+                                                                    <span class="name">
+                                                                        <span class="SpanInput">子章</span>
+                                                                        <span class="SummaryEditButton"><i class="Icon octicon octicon-pencil"></i></span>
+                                                                    </span>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div class="TreeEntry">
+                                                        <div class="inner" draggable="true" style="padding-left: 10px;">
+                                                            <!-- react-text: 176 --><!-- /react-text --><span
+                                                                class="name"><span class="SpanInput">第二章</span><span
+                                                                    class="SummaryEditButton"><i
+                                                                        class="Icon octicon octicon-pencil"></i></span></span>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="SummaryActions">
+                                                <button type="button" class="btn btn-marketing-primary btn-md"
+                                                        role="button"><!-- react-text: 183 --><!-- /react-text -->
+                                                    <!-- react-text: 184 --> <!-- /react-text --><i
+                                                        class="Icon octicon octicon-plus"></i>
+                                                    <!-- react-text: 186 --> Add an article<!-- /react-text --></button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="resize-bar-h"></div>
+                        </div>
+                        <div class="GridEntry" style="top: 0%; left: 15%; height: 100%; width: 85%;">
+                            <div class="entry-inner">
+                                <div class="Workspace">
+                                    <div class="ScrollArea" name="editor">
+                                        <div class="EditorContainer">
+                                            <div class="DocumentEditor textAlignment-left fontSize-default">
+                                                <div class="DocumentEditor-FrontMatter-Container">
+                                                    <div class="DocumentEditor-FrontMatter">
+                                                        <div class="toggler">edit metadata</div>
+                                                    </div>
+                                                </div>
+                                                <div>
+                                                    <div data-slate-editor="true" contenteditable="true"
+                                                         class="SlateEditor" autocorrect="true" spellcheck="true"
+                                                         role="textbox"
+                                                         style="outline: none; white-space: pre-wrap; word-wrap: break-word; -webkit-user-modify: read-write-plaintext-only;">
+                                                        <div class="DocumentEditor-HeadingNode"><h1 data-key="24"><span
+                                                                    data-key="23"><span data-offset-key="23-0">这是第一章</span></span>
+                                                            </h1>
+                                                            <div class="DocumentEditor-HeaderAnchor"
+                                                                 contenteditable="false">
+                                                                <div class="popover-container">
+                                                                    <div class="Anchor"><i
+                                                                            class="Icon octicon octicon-pin"></i></div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                        <p data-key="27">
+                                                            <span data-key="26">
+                                                            <span data-offset-key="26-0"><br></span>
+                                                            </span>
+                                                        </p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="FooterToolbar">
+                        <div class="btn-toolbar ">
+                            <button type="button" class="btn btn-link btn-md HistoryButton" role="button">
+                                <!-- react-text: 144 --><!-- /react-text --><!-- react-text: 145 -->
+                                <!-- /react-text --><span><!-- react-text: 293 -->Updated <!-- /react-text --><span>5 days ago</span></span>
+                            </button>
+                            <span></span>
+                            <div class="btn-group dropdown dropup">
+                                <button type="button" class="btn btn-secondary btn-md dropdown-toggle" role="button">
+                                    <!-- react-text: 150 --><!-- /react-text --><!-- react-text: 151 -->
+                                    <!-- /react-text --><!-- react-text: 152 -->?<!-- /react-text --></button>
+                            </div>
+                        </div>
+                    </div><!-- react-empty: 153 --></div>
+            </div>
+        </div>
+        <div class="DialogsContainer"></div>
+        <div id="modals"></div>
+    </div>
+</div>
+
+<script>
+        window.ASSETS_PATH = "/editor/assets";
+    //                window.EDITOR = {"host":"https://git.gitbook.com/","repository":"huaiguoguo/test","username":"huaiguoguo","token":"e41a6d70-65a3-4481-b6fe-96b9d1246a7d","storageKey":"editor.59b0edb38085e3002dbe3f7c","origin":"/book/huaiguoguo/test/details","defaultBranch":"master","committer":{"name":"火柴","email":"290559038@qq.com"},"rousseau":{"host":"https://rousseau.gitbook.com"},"book":{"title":"test","id":"huaiguoguo/test","url":"https://www.gitbook.com/book/huaiguoguo/test"},"api":{"host":"https://api.gitbook.com/","username":"huaiguoguo","token":"e41a6d70-65a3-4481-b6fe-96b9d1246a7d"},"analytics":{"debug":false,"distinctID":"57bd09e19427bd100018bc89"}};
+</script>
+<script type="text/javascript" src="/gitbook/assets/MathJax.js"></script>
+<script type="text/javascript" src="/gitbook/assets/application.js"></script>
+</body>
+</html>
